0613f517b2d27161b0e30a146ede919a203021c1
PercheEtienne <etienne.perche@ig2i.centrale.fr>
2021-12-09 22:09:33 +0100

    Add Drag functions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const candiesColors = ['blue','green','orange','purple','red','yellow']
 function App() {
 
   const [gameBoard,setGameBoard] = useState([])
+  const [candyBeingDragged,setCandyBeingDragged] = useState(null)
+  const [candyBeingReplaced,setCandyBeingReplaced] = useState(null)
 
   const createGameBoard = () => {
     let randomColors = []
@@ -40,7 +42,7 @@ function App() {
 
   const checkFor3inRow = () => {
     const notValidIndex = []
-    for (let i = 0; i<(width*width)-(2*width) ; i++){
+    for (let i = 0; i<(width*width) ; i++){
       const rowOf3 = [i,i+1,i+2]
       const currentColor = gameBoard[i]
       for (let j = 1;j<(width*width);j++){
@@ -57,7 +59,7 @@ function App() {
 
   const checkFor4inRow = () => {
     const notValidIndex = []
-    for (let i = 0; i<(width*width)-(2*width) ; i++){
+    for (let i = 0; i<(width*width) ; i++){
       const rowOf4 = [i,i+1,i+2,i+3]
       const currentColor = gameBoard[i]
       for (let j = 1;j<(width*width);j++){
@@ -111,6 +113,40 @@ function App() {
        return () => clearInterval(timer)
       },[checkFor4inRow,checkFor4inColumn,checkFor3inRow,,checkFor3inColumn,moveIntoSquareBelow,gameBoard])
 
+  const dragStart = (e) =>{
+    //console.log(e.target)
+    console.log("drag start")
+    setCandyBeingDragged(e.target)
+  }
+
+  const dragDrop = (e) =>{
+    //console.log(e.target)
+    console.log("drag drop")
+    setCandyBeingReplaced(e.target)
+  }
+
+  const dragEnd = (e) =>{
+    //console.log(e.target)
+    console.log("drag end")
+    const idCandyBeingDragged = parseInt(candyBeingDragged.getAttribute('data-id'))
+    const idCandyBeingReplaced = parseInt(candyBeingReplaced.getAttribute('data-id'))
+
+    if (idCandyBeingReplaced === idCandyBeingDragged+1
+        || idCandyBeingReplaced === idCandyBeingDragged-1
+        || idCandyBeingReplaced === idCandyBeingDragged+width
+        || idCandyBeingReplaced === idCandyBeingDragged-width
+    ){ //TODO : vérifier pour les extrèmes
+      gameBoard[idCandyBeingReplaced] = candyBeingDragged.style.backgroundColor
+      gameBoard[idCandyBeingDragged] = candyBeingReplaced.style.backgroundColor
+    }
+    else {
+      console.log("Unauthorized drag")
+      setCandyBeingReplaced(null)
+      setCandyBeingDragged(null)
+    }
+  }
+
+
   return (
     <div className="app" >
       <div className="game">
@@ -119,7 +155,14 @@ function App() {
               key={index}
               style={{backgroundColor: candiesColors}}
               alt={candiesColors}
-
+              data-id={index}
+              draggable={true}
+              onDragStart={dragStart}
+              onDragOver={(event => event.preventDefault())}
+              onDragEnter={(event => event.preventDefault())}
+              onDragLeave={(event => event.preventDefault())}
+              onDrop={dragDrop}
+              onDragEnd={dragEnd}
             />
           ))}
       </div>
